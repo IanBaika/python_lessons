ed94cc0138ea00f34b64d6ee37ed836ce52b63c4
IanBaika <76169723+IanBaika@users.noreply.github.com>
2020-12-22 19:21:36 +0600

    Update lesson2 hw.py

--- lesson2 hw.py
+++ lesson2 hw.py
@@ -19,3 +19,27 @@ phrase = input('Введите фразу или слово: ')
 if len(phrase) > 10:
     print('Нельзя вводить так много символов')
 else: print('Вы ввели допустимое количество символов')
+    
+# Практическое задание с 48й строки: 
+number = 16
+second_number = not 1
+first_array = [1, 2, 3]
+second_array = [1, 2]
+
+if number > 15:
+    print("1")
+
+if first_array:
+    print("2")
+
+if len(second_array) == 2:
+    print("3")
+
+if len(first_array) + len(second_array) == 5:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
