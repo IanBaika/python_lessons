2029fe4b02dff1136710df1c3080e752bc769573
IanBaika <76169723+IanBaika@users.noreply.github.com>
2021-01-16 19:30:28 +0600

    Update lesson 7 hw.py

--- lesson 7 hw.py
+++ lesson 7 hw.py
@@ -1,18 +1,32 @@
 # -*- coding: utf8 -*-
-class Worker: #создаю класс Worker
-    salary = 3000 # ставлю зарплату по умолчанию 3000
-    def work(self): #задаю ему метод work
-        print('Рабочий делает ручную работу. Зарплата -', self.salary, 'сом.') # Возвращаю информацию
-class Driver(Worker): #создаю наследующи от класса Workek класс Driver
-    has_car = True #задаю ему неизменяемое свойство
-    def __init__(self,salary): #зарплата задается пользователем
+class Worker:  # создаю класс Worker
+    def __init__(self,salary = 3000): # задаю зарплату по умолчанию
         self.salary = salary
-    def work(self): #Возвращаю информацию
-        print('Водитель водит машину. Зарплата -', self.salary, 'сом.', self.has_car)
-    def __del__(self): #создаю деструктор
+
+    def __del__(self):  # создаю деструктор
+        print('Рабочий уволен')
+
+
+    def work(self):  # задаю ему метод work
+        print('Рабочий делает ручную работу. Зарплата -', self.salary, 'сом.')  # Возвращаю информацию
+
+
+class Driver(Worker):  # создаю наследующий от класса Workek класс Driver
+    __has_car = True  # задаю ему неизменяемое свойство
+
+    def __init__(self, salary=5000):  # задаю зарплату по умолчанию
+        self.salary = salary
+
+    def work(self):  # Возвращаю информацию
+        print('Водитель водит машину. Зарплата -', self.salary, 'сом.', self.__has_car)
+
+    def __del__(self):  # создаю деструктор
         print('Водитель уволен')
-# Создаю объекты класса, вызываю им метод work
-Adam = Driver(10000)
+
+# Создаю объекты класса Driver, вызываю им метод work
+Adam = Driver(10000) 
 Adam.work()
+
+# Создаю объекты класса Worker, вызываю им метод work
 Ravshan = Worker()
 Ravshan.work()
